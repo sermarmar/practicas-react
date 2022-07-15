a119eec29227fdfba8826b0b2626dd1fe672cec6
sermarmar <sermarmar1995@gmail.com>
2022-07-15 19:22:33 +0200

    Prueba

---
+++ src/ejercicios/async.js
@@ -0,0 +1,37 @@
+/*const getImagenPromesa = () => {
+    return new Promise(resolve => resolve('https://jasldjfewalsas.com'));
+}
+
+getImagenPromesa().then(console.log)*/
+
+//Asincrono
+const getImagen = async() => {
+
+    return 'https://jasldjfewalsas.com';
+
+}
+
+getImagen().then(console.log)
+
+//Sincrono
+const getGif = async() => {
+
+    try {
+        const apiKey = 'C1GDcgdRovtdHV4ZEi4VgrwVnJKagymo';
+        const resp = await (await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`));
+        const { data } = await resp.json();
+
+        const { url } = data.images.original;
+
+        const img = document.createElement('img');
+        img.src = url;
+
+        document.body.append(img);
+    } catch (error) {
+        //Manejo de error
+        console.error(error);
+    }
+
+}
+
+getGif().then(console.log)
